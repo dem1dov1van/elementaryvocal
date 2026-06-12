ba04fe5d88e5b98e512b72d66c082b2aa513520b
dem1dov1van <demidov.i@selectel.ru>
2026-06-12 18:48:53 +0300

    favicon & deploy yaml

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -4,14 +4,19 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+    { rel: 'shortcut icon', href: '/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { name: 'apple-mobile-web-app-title', content: 'Elementary' },
+    { rel: 'manifest', href: '/site.webmanifest' }
   ],
   htmlAttrs: {
     lang: 'ru'
   }
 })
 
-const title = 'Elementaryvocal - школа эстрадного вокала'
+const title = 'Elementaryvocal - школа вокала Александры Родиной'
 const description = 'Добро пожаловать в вокальную школу Elementeryvocal. Александра Родина помогает вокалистам звучать свободнее, комфортнее и увереннее.'
 
 useSeoMeta({
